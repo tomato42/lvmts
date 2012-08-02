996b054e01bc6025283fe353a081b9b7ed7347a2
Hubert Kario <kario@wit.edu.pl>
2012-08-02 21:15:03 +0200

    properly select extents in both HOT and COLD case

--- volumes.c
+++ volumes.c
@@ -68,7 +68,12 @@ int extents_selector(struct extent_stats *es, struct extents **ret,
 
         int tier = get_extent_tier(pp, lv_name, &es->extents[i]);
 
-        if ((hot_cold == ES_HOT && max_tier > tier) || max_tier < tier) {
+        if ((hot_cold == ES_HOT && max_tier < tier) ||
+            (hot_cold == ES_COLD && max_tier == tier)) {
+
+            if (hot_cold == ES_COLD) {
+                printf("max_tier: %i, tier: %i\n", max_tier, tier);
+            }
             (*ret)->extents[(*ret)->length] = &es->extents[i];
             (*ret)->length++;
         }
