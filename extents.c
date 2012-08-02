8e6e1ceddf974a16081545929407f3847d3508d9
Hubert Kario <kario@wit.edu.pl>
2012-08-02 19:35:41 +0200

    fix parameter mixup in get_avaiable_space()

--- extents.c
+++ extents.c
@@ -154,8 +154,8 @@ get_avaiable_space(struct program_params *pp, const char *lv_name, int tier)
       return avaiable_space;
 
     long int used_space = get_used_space_on_pv(
-        get_volume_lv(pp, lv_name),
         get_volume_vg(pp, lv_name),
+        get_volume_lv(pp, lv_name),
         pv_name);
 
     used_space *= get_pe_size(get_volume_vg(pp, lv_name));
